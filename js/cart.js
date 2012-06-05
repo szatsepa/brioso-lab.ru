67093da45d9247d644c525082099f0802e2fda9b
szatsepa <arcady.1254@gmail.com>
2012-06-05 19:39:00 +0300

    order

--- js/cart.js
+++ js/cart.js
@@ -4,6 +4,8 @@
 
 $(document).ready(function(){
     
+    var month = new Array('- 01 -','- 02 -','- 03 -','- 04 -','- 05 -','- 06 -','- 07 -','- 08 -','- 09 -','- 10 -','- 11 -','- 12 -')
+    
         if (document.readyState != "complete"){
 		setTimeout( arguments.callee, 200 );
 		return;
@@ -15,10 +17,60 @@ $(document).ready(function(){
         user_id = parseInt(id); 
         Cart(user_id);
         }
+        
+        $("#order").mouseover(function(){
+            $("#order").css('color', 'blueviolet');
+        });
+        $("#order").mouseout(function(){
+            $("#order").css('color', 'black');
+        });
+        $("#order").mousedown(function(){
+            $("#content").css('display', 'none');
+            createOrder();
+        });
+        
+        $("#to_order").mouseover(function(){
+            $("#to_order").css('color', 'blueviolet');
+        });
+        $("#to_order").mouseout(function(){
+            $("#to_order").css('color', 'black');
+        });
+        $("#to_order").mousedown(function(){
+//            saveOrder();
+            var email = $("#email").attr('name'); 
+            alert(email);
+        });
+        
+        function createOrder(){
+            $.ajax({
+                url: 'http://brioso-lab.ru/query/num_order.php',
+                type: 'post',
+                dataType: 'json',
+                success:function(data){
+                    var now = new Date();
+                    var num = data['num']+1;
+                    if(!num)num=1;
+                    $("#order_form").css('display','block');
+                    $("#order_title").text("Заказ №"+num+" от "+now.getDate()+" "+month[now.getMonth()]+" "+now.getFullYear()+" г.");
+                    $("#shipment").focus();
+                }
+            });
+        }
+        
+        function saveOrder(){
+            var email = $("#email").val();
+            var order = {email:email,shipment:null};
+//            for(var i in my_cart){ 
+//                
+//                for(var ii in my_cart[i]){ 
+//                    str += ii+";\n";
+//                }
+//            }
+//            alert(order['email']);
+        }
+        
+        
 //        $('div').css('outline', '1px solid blue');
-//
-//        $("#change").mousedown(function(eval){
-//            alert(this['name']);
-//        })
+
 });
 
--- main/chambers.php
+++ main/chambers.php
@@ -7,12 +7,17 @@ $summ_list = 0.00;
 ?>
 <script type="text/javascript">
     user_id = <?php echo $_SESSION[id];?>;
+    var my_cart = new Array();
 </script>
 <div id="content">
     <?php 
     $n = 0;
     foreach ($cart_array as $value) {
         ?>
+    <script type="text/javascript">
+    var tmp = <?php echo json_encode($value);?>;
+    my_cart.push(tmp);
+    </script>
     <div class="cart">
        <form id="<?php echo "form_$n";?>">
             <div class="cart_img">
@@ -48,7 +53,86 @@ $summ_list = 0.00;
         $n++;
     }
     ?>
-    <div style="position: relative;width: 860px;height: 35px;margin: 6px auto;text-align: right;padding-right: 86px;">
-        <p style="font-size: 16px;font-weight: bold;color: black;">Итого:&nbsp;&nbsp;<?php $str_out = intval("$summ_list"); echo $str_out.".00";?></p> 
+    <div id="itogo">
+        <p id="in_itogo">Итого:&nbsp;&nbsp;<?php $str_out = intval("$summ_list"); echo $str_out.".00 p.";?></p>
+        <p><a name="" id="order">Оформить заказ</a></p>
     </div>
+</div>
+<div id="order_form">
+    <div style="position:relative; width: 600px;height:27px;">
+        <p id="order_title"></p>
+    </div>
+    <div class="order_header" style="font-size:16px;font-weight:bold;color: black;position:relative;margin:0 auto;width:92%;height:47px;">
+            <div style="position:relative;float:left;width:43px;text-align:center;">
+                <p style="">№</p>
+            </div>
+            <div style="position:relative;float:left;width:543px;text-align:center;">
+                <p>Наименование</p>
+            </div>
+            <div style="position:relative;float:left;width:52px;text-align:center;">
+                <p style="">Колич</p>
+            </div>
+            <div style="position:relative;float:left;width:143px;text-align:center;">
+                <p style="">Цена</p>
+            </div>
+    </div>
+    <div id="order_body">
+        <?php 
+        $n = 1;
+        foreach ($cart_array as $value) {
+            ?>
+        <div id="order_row_<?php echo $n;?>" style="font-size:14px;color: black;position:relative;margin:0 auto;width:92%;height:47px;">
+            <div style="position:relative;float:left;width:43px;text-align:center;">
+                <p><?php echo $n;?></p>
+            </div>
+            <div style="position:relative;float:left;width:543px;text-align:left;">
+                <p id="name_<?php echo $n;?>"><?php echo $value[name];?></p>
+            </div>
+            <div style="position:relative;float:left;width:52px;text-align:center;">
+                <p id="amount_<?php echo $n;?>" style=""><?php echo $value[amount];?></p>
+            </div>
+            <div style="position:relative;float:left;width:143px;text-align:center;">
+                <p id="cost_<?php echo $n;?>" style=""><?php echo $value[cost];?></p>
+                 
+            </div>
+            <div style="visibility:hidden;" >
+                <p id="artikul_<?php echo $n;?>"><?php echo $value[artikul];?></p>
+            </div>
+        </div>
+        <?php
+        $n++;
+        }
+        ?>
+    </div>
+    <div class="order_footer"  style="font-weight:normal;position:relative; width: 720px;height:270px;margin: 0 auto;text-align:left;">
+        <div id="address">
+            <p>
+                <label>Адрес доставки:&nbsp;&nbsp;&nbsp;&nbsp;</label><input id="shipment" type="text" value="" size="72"/>
+            </p>
+        </div>
+        <div id="o_email">
+            <p><label>Email:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label><input id="email" type="text" name="joppa" value="ЖОППА" size="72"/></p>
+        </div>
+        <div id="o_phone">
+            <p>
+                <label>Телефон:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label><input id="phone" type="text" value="" size="72"/>
+            </p>
+        </div>
+        <div id="o_comm">
+            <p>
+                <label>Пожелания:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label><textarea id="comment" cols="72" rows="6"></textarea>
+            </p>
+        </div>
+        <div id="o_order" style="width:520px;margin-top: 12px;">
+            <p id="to_order" style="text-align: right;font-size: 18px;font-weght:bold;color: black;text-decoration:underline;cursor:pointer;">
+                Заказать
+            </p>
+        </div>
+     </div>
+    <div id="o_order" style="width:100%;height:24px;">
+<!--            <p id="to_order" style="text-align: right;font-size: 18px;font-weght:bold;color: black;text-decoration:underline;cursor:pointer;">
+                Заказать
+            </p>-->
+        </div>
+    
 </div>
